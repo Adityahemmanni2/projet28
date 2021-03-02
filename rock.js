740e3f967bd4a13c0942c26823f82f621454750d
Adityahemmanni2 <75521474+Adityahemmanni2@users.noreply.github.com>
2021-03-02 15:35:22 +0530

    Add files via upload

---
+++ rock.js
@@ -0,0 +1,13 @@
+
+class rock{
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("images/stone.png");
+    }
+  
+    display() {
+      //this.body.position.x = mouseX;
+      //this.body.position.y = mouseY;
+      super.display();
+    }
+  }
